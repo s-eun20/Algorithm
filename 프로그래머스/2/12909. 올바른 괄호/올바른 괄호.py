eb04ce022a926fee9ac453a82e9de43ac1463554
s-eun20 <109144010+s-eun20@users.noreply.github.com>
2024-04-17 17:56:38 +0900

    [level 2] Title: 올바른 괄호, Time: 7.25 ms, Memory: 10.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -0,0 +1,15 @@
+def solution(s):
+    stack = []
+    for i in s :
+        if i == "(" :
+            stack.append(i)
+        elif i == ")" :
+            if stack :
+                stack.pop()
+            else :
+                return False
+            
+    if stack :
+        return False
+    else :
+        return True
