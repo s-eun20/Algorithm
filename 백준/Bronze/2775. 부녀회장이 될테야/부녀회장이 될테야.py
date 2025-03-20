88b9c98c8603b3363dee0f96867fcbd14b736b1e
s-eun20 <109144010+s-eun20@users.noreply.github.com>
2025-03-20 15:26:49 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
@@ -1,10 +1,17 @@
-num = int(input())
+T = int(input())
 
-for i in range(num) :
-  a=int(input())
-  b=int(input())
-  floor = [j for j in range(1,b+1)]
-  for l in range(a) :
-    for m in range(1,b) :
-      floor[m] +=floor[m-1]
-  print(floor[b-1])
+for _ in range(T) :
+    K = int(input())
+    N = int(input())
+    
+    apt = [[0]*(N+1) for _ in range(K+1)]
+    
+    for i in range(1,N+1):
+        apt[0][i] = i
+        
+    for i in range(1,K+1):
+        for j in range(1,N+1):
+            apt[i][j] = apt[i][j-1]+apt[i-1][j]
+    
+    
+    print(apt[K][N])
