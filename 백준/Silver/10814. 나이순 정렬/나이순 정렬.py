d361cbed5743bb764ee29017659691d1d25d02de
s-eun20 <109144010+s-eun20@users.noreply.github.com>
2025-03-26 20:54:25 +0900

    [Silver V] Title: 나이순 정렬, Time: 300 ms, Memory: 52636 KB -BaekjoonHub

---
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
@@ -0,0 +1,12 @@
+import sys
+n=int(sys.stdin.readline())
+
+member=[]
+
+for i in range(n) :
+    member.append(list(sys.stdin.readline().split()))
+    
+member.sort(key=lambda a:int(a[0]))
+
+for i in range(n):
+    print(member[i][0],member[i][1])
