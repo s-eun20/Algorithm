da55a47733b417b4158aa5ab5b520e54408b3671
s-eun20 <109144010+s-eun20@users.noreply.github.com>
2025-03-26 18:35:06 +0900

    [Silver V] Title: 영화감독 숌, Time: 596 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -1,13 +1,14 @@
-n = int(input())
+N = int(input())
 
 result = 666
+
 count = 0
 
 while True :
-    if '666' in str(result) :
-        count +=1
-    if count == n :
-        print(result)
-        break
-        
-    result+=1
+    if '666' in str(result):
+        count +=1     
+        if count == N :
+            print(result)
+            break
+    result += 1
+     
