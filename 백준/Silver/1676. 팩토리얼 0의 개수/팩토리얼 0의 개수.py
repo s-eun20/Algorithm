918319293f3df344c79037689e9d7358338fd98b
s-eun20 <109144010+s-eun20@users.noreply.github.com>
2025-03-26 18:42:22 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 36 ms, Memory: 32456 KB -BaekjoonHub

--- 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.py
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.py
@@ -1,19 +1,19 @@
 n = int(input())
 
-result = 1
-for i in range(2, n + 1): 
-    result *= i
-    
-    
-total_str = str(result)[::-1] 
-
 count = 0
-for i in total_str:
-    if i == '0':
-        count += 1  
-    else:
-        break  
+def factorial(n):
+    if n >1 :
+        return n*factorial(n-1)
+    else :
+        return 1
+    
+num = str(factorial(n))[::-1]
 
+for i in num :
+    if i == '0':
+        count+=1
+    else :
+        break
+        
 print(count)
 
-    
