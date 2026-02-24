d9d80147c02faa79de5cc2ba736857302a9d164e
s-eun20 <109144010+s-eun20@users.noreply.github.com>
2026-02-24 16:34:48 +0900

    [Silver IV] Title: 괄호, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.py
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -1,23 +1,24 @@
-def is_valid_parenthesis(parenthesis):
+import sys
+num = int(sys.stdin.readline())
+
+
+for _ in range(num) :
     stack = []
-
-    for p in parenthesis:
-        if p == '(':
-            stack.append(p)
-        else:
+    string = sys.stdin.readline().strip()
+    is_vaild = True
+    
+    for j in string :
+        if j == "(" :
+            stack.append(j)
+        elif j == ")" :
             if not stack:
-                return "NO"
+                is_vaild = False
+                break
             stack.pop()
+           
+    if is_vaild and not stack :
+        print("YES")
+    else :
+        print("NO")
+    
 
-    if not stack:
-        return "YES"
-    else:
-        return "NO"
-
-# 입력 받기
-test_cases = int(input())
-
-# 각 테스트 케이스에 대해 결과 출력
-for _ in range(test_cases):
-    parenthesis = input()
-    print(is_valid_parenthesis(parenthesis))
