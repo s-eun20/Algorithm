2e454a40f3392d74361332ea5d3e40df12bc54c9
s-eun20 <109144010+s-eun20@users.noreply.github.com>
2026-03-11 19:02:27 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -1,17 +1,9 @@
-staure = []
-for _ in range(9) :
-  staure.append(int(input()))
+from itertools import combinations
 
-staure.sort()
+heights = [int(input()) for _ in range(9)]
 
-staure_sum = sum(staure)
-
-for i in range(len(staure)) :
-  for j in range(i+1,len(staure)) :
-    if staure_sum - staure[i] - staure[j] == 100 :
-      for k in range(len(staure)) :
-        if k != i and k != j :
-          print(staure[k])
-        else :
-          pass
-      exit()
+for comb in combinations(heights,7) :
+    if sum(comb) == 100 :
+        for h in sorted(comb):
+            print(h)
+        break
