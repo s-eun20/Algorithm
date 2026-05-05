0c8a98ba6b077fb87f1e915f4191481cb797f6f3
s-eun20 <109144010+s-eun20@users.noreply.github.com>
2026-05-05 21:02:53 +0900

    [level 0] Title: 수 조작하기 2, Time: 25.87 ms, Memory: 14.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.py
@@ -0,0 +1,14 @@
+def solution(numLog):
+    result = []
+    for i in range(1,len(numLog)) :
+        if numLog[i] - numLog[i - 1] == 1 :
+            result.append("w")
+        elif numLog[i] - numLog[i - 1] == -1 :
+            result.append("s")
+        elif numLog[i] - numLog[i - 1] == 10 :
+            result.append("d")
+        elif numLog[i] - numLog[i - 1] == -10 :
+            result.append("a")
+    
+    return ''.join(result)
+        
