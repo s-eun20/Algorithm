4bc6caa04003c311e7e7b3175cfb6dbba8755911
s-eun20 <109144010+s-eun20@users.noreply.github.com>
2026-07-05 19:20:56 +0900

    [level 1] Title: 추억 점수, Time: 0.71 ms, Memory: 12.1 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.py
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.py
@@ -1,10 +1,16 @@
 def solution(name, yearning, photo):
-    answer = [] # answer list
-    for people in photo: # iterate photos
-        total_score = 0 # set total score to 0
-        for person in people: # iterate people in photo
-            if person in name: # if person in the name list
-                idx = name.index(person) # find the index of the person
-                total_score += yearning[idx] # add person's yearning score to the total score
-        answer.append(total_score) # add the total score to the answer list
-    return answer # return the answer list
+    count = {}
+    result = []
+    
+    for i in range(len(name)) :
+        count[name[i]] = yearning[i]
+        
+    for p in photo :
+        num = 0
+        for word in p :
+            if word in count.keys() :
+                num += count[word]
+        result.append(num)
+            
+    return result
+            
