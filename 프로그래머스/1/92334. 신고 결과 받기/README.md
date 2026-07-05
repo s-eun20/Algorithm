160593ae2add1b179779b6ceafb1096eaf71cb3b
s-eun20 <109144010+s-eun20@users.noreply.github.com>
2026-07-05 19:06:00 +0900

    [level 1] Title: 신고 결과 받기, Time: 97.17 ms, Memory: 39.7 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/README.md
+++ 프로그래머스/1/92334. 신고 결과 받기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 신고 결과 받기 - 92334 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92334?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92334) 
 
 ### 성능 요약
 
-메모리: 38.6 MB, 시간: 1410.96 ms
+메모리: 39.7 MB, 시간: 97.17 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 20일 22:24:31
+2026년 07월 05일 19:05:57
 
 ### 문제 설명
 
--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.py
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.py
@@ -1,28 +1,27 @@
 def solution(id_list, report, k):
-    # 1. 중복 신고 제거
-    report = list(set(report))
-
-    # 2. 신고당한 횟수 저장용 딕셔너리
-    reported_count = {user: 0 for user in id_list}
-
-    # 3. 각 유저가 누구를 신고했는지 저장
-    report_by_user = {user: [] for user in id_list}
-
-    for r in report:
-        reporter, reported = r.split()
-        reported_count[reported] += 1
-        report_by_user[reporter].append(reported)
-
-    # 4. 정지된 유저 리스트
-    banned_users = [user for user, count in reported_count.items() if count >= k]
-
-    # 5. 각 유저가 받은 메일 수 계산
+    count = {}
+    name = {}
     result = []
-    for user in id_list:
-        count = 0
-        for reported in report_by_user[user]:
-            if reported in banned_users:
-                count += 1
-        result.append(count)
-
+    
+    for id in id_list:
+        count[id] = count.get(id,0)
+        name[id] = name.get(id,0)
+    
+    
+    
+    report = list(set(report))
+    
+    for r in report :
+        user, target = r.split()
+        count[target] +=1
+            
+    for r in report:
+        user, target = r.split()
+        
+        if count[target] >= k :
+            name[user] += 1
+            
+    
+    result = list(name.values())
     return result
+            
