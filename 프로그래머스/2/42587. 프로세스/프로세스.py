9b22054ff8eca258e5f79c51208368f48c2f8d38
s-eun20 <109144010+s-eun20@users.noreply.github.com>
2026-07-09 14:41:14 +0900

    [level 2] Title: 프로세스, Time: 0.17 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.py
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -1,19 +1,28 @@
+from collections import deque
 def solution(priorities, location):
-    # 1) (우선순위, 원래 인덱스) 형태로 큐 만들기
-    queue = [(p, i) for i, p in enumerate(priorities)]
+    queue = deque()
     
-    printed = 0  # 출력된 문서 개수(=순서)
-
+    #(원래 인덱스, 우선순위) 저장
+    for i,priority in enumerate(priorities) :
+        queue.append((i,priority))
+        
+    answer = 0
+    
     while queue:
-        # 2) 맨 앞 문서 꺼내기
-        cur_p, cur_i = queue.pop(0)
-
-        # 3) 지금 큐 안에 cur_p 보다 더 큰 우선순위가 있는지 확인
-        if any(cur_p < p for p, _ in queue):
-            # 더 큰 게 있으면 뒤로 보냄
-            queue.append((cur_p, cur_i))
-        else:
-            # 4) 없으면 출력 처리
-            printed += 1
-            if cur_i == location:
-                return printed
+        index,priority = queue.popleft()
+        
+        flag = False
+        
+        for _, p in queue:
+            if p > priority :
+                flag = True
+                break
+                
+        if flag :
+            queue.append((index,priority))
+            
+        else :
+            answer += 1
+            
+            if index == location :
+                return answer
