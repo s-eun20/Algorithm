a25f016f9c8bf2aeaf7907408184188903931b85
s-eun20 <109144010+s-eun20@users.noreply.github.com>
2026-07-16 13:04:22 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 42.82 ms, Memory: 30.3 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
 
 ### 성능 요약
 
-메모리: 30.6 MB, 시간: 44.24 ms
+메모리: 30.3 MB, 시간: 42.82 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 07월 05일 17:45:45
+2026년 07월 16일 13:04:18
 
 ### 문제 설명
 
--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,13 +1,13 @@
+
 def solution(participant, completion):
+    answer = {}
     
-    parts = {}
-
-    for part in participant:
-        parts[part] = parts.get(part,0) + 1
-
-    for person in completion:
-        parts[person] -= 1
+    for part in participant :
+        answer[part] = answer.get(part,0) + 1
         
-    for key,value in parts.items():
-        if value == 1 :
+    for name in completion :
+        answer[name] -= 1
+        
+    for key,value in answer.items() :
+        if value > 0  :
             return key
