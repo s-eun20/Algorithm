0904b1daf41029eb45c2974c0835c06a027158be
s-eun20 <109144010+s-eun20@users.noreply.github.com>
2026-07-16 13:07:56 +0900

    [level 1] Title: 폰켓몬, Time: 0.54 ms, Memory: 12.5 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,9 +1,3 @@
 def solution(nums):
-    
-    set_nums = set(nums)
-    len_nums = len(set_nums)
-    
-    return min(len(nums)//2 , len_nums)
-
-
-# 중복제거와 몇마리인지 중요ㅛ함
+    answer = min (len(set(nums)), len(nums)//2)
+    return answer
