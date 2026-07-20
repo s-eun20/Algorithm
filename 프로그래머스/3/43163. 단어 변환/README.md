ffcc54fa8b0fb4b76de72cdb56bce6cc1461f216
s-eun20 <109144010+s-eun20@users.noreply.github.com>
2026-07-20 16:32:28 +0900

    [level 3] Title: 단어 변환, Time: 0.16 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/README.md
+++ 프로그래머스/3/43163. 단어 변환/README.md
@@ -1,10 +1,10 @@
 # [level 3] 단어 변환 - 43163 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43163?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43163) 
 
 ### 성능 요약
 
-메모리: 11.3 MB, 시간: 0.17 ms
+메모리: 11.3 MB, 시간: 0.16 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 07월 19일 00:15:10
+2026년 07월 20일 16:32:24
 
 ### 문제 설명
 
--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -1,27 +1,28 @@
 from collections import deque
 def solution(begin, target, words):
+    
+    visited = [False] * len(words)
     queue = deque()
     queue.append((begin,0))
     
-    visited = [False] * len(words)
-    
     while queue :
-        current, count = queue.popleft()
+        current,count = queue.popleft()
         
         if current == target :
             return count
-        
         for i in range(len(words)) :
             if visited[i] :
                 continue
                 
-            different = 0
+            diff = 0
             
-            for a,b in zip(current, words[i]):
-                if a != b :
-                    different += 1
+            for a,b in zip(current,words[i]) :
+                if a!=b :
+                    diff += 1
                     
-            if different == 1 :
+            if diff == 1 :
                 visited[i] = True
                 queue.append((words[i],count+1))
+                
     return 0
+    
