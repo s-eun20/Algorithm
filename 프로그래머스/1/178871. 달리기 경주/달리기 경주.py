c970c80652c398779f738c0505418f4230566c9e
s-eun20 <109144010+s-eun20@users.noreply.github.com>
2026-07-21 20:21:55 +0900

    [level 1] Title: 달리기 경주, Time: 553.18 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.py
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.py
@@ -1,16 +1,17 @@
 def solution(players, callings):
     rank = {}
-
-    for i, player in enumerate(players):
-        rank[player] = i
-
-    for calling in callings:
-        current_rank = rank[calling]
-        front_player = players[current_rank - 1]
-
-        players[current_rank - 1], players[current_rank] = players[current_rank], players[current_rank - 1]
-
-        rank[calling] -= 1
-        rank[front_player] += 1
-
+    answer = []
+    
+    for i in range(len(players)) :
+        rank[players[i]] = i
+    
+    for calling in callings :
+        idx = rank[calling]
+        front = players[idx-1]
+        players[idx] = front
+        players[idx-1] = calling
+        rank[calling] -=1
+        rank[front] += 1
+    
+    
     return players
